1f90acdec964f7642e8cb816fa66007f7c90d242
Arpit Mishra <arpitmishra61@gmail.com>
2023-04-15 14:51:21 +0530

    updated node version

--- server.js
+++ server.js
@@ -2,20 +2,10 @@ const express = require("express");
 const app = express();
 
 const images = require("./files/images").reverse();
-const mongoose = require("mongoose");
 const cors = require("cors");
 require("./Modals/counter");
 
 app.use(cors());
-let dbUrl =
-  "mongodb+srv://arpitmishra:arpitskaju@cluster0-6m0pr.mongodb.net/test";
-mongoose
-  .connect(dbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("DB Connected"))
-  .catch((err) => console.log(err));
 
 app.use(
   express.json({
@@ -24,8 +14,6 @@ app.use(
 );
 
 app.use(cors());
-
-const VisitorCounter = mongoose.model("Counter");
 
 const port = process.env.PORT || 9000;
 app.listen(port, () => console.log("Server is started at port: " + port));
@@ -40,15 +28,6 @@ app.get("/imageNames", (req, res) => {
 
 app.get("/thumbnailimage/:id", (req, res) => {
   res.sendFile(__dirname + `/public/images/${images[req.params.id]}`);
-});
-
-app.get("/visitorCount", (req, res) => {
-  VisitorCounter.find({ name: "magzine" })
-    .then((data) => {
-      console.log(data);
-      res.json(data[0].count);
-    })
-    .catch((err) => console.log(err));
 });
 
 app.post("/visitorCount/:count", (req, res) => {
